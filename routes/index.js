dcbeb05d537d56650bdd29c36a12e6849f56aad9
MathisPipart <117630102+MathisPipart@users.noreply.github.com>
2024-12-13 15:17:36 +0100

    setting up express architecture with different page

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('pages/index', { title: 'My Chat' });
+  res.render('pages/index', { title: 'Home' });
 });
 
-router.get('/movies', function(req, res, next) {
-  res.render('pages/movies', { title: 'Movie' });
+router.get('/genres', function(req, res, next) {
+  res.render('pages/genres', { title: 'Genres' });
+});
+
+router.get('/releases', function(req, res, next) {
+  res.render('pages/releases', { title: 'Releases' });
+});
+
+router.get('/languages', function(req, res, next) {
+  res.render('pages/languages', { title: 'Languages' });
+});
+
+
+router.get('/chat', function(req, res, next) {
+  res.render('pages/chat', { title: 'Chat' });
 });
 
 module.exports = router;
